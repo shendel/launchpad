544bd45124a45fbc1743c75d68d38fda339dd5f2
Scroll Sale <149031380+Scrollsale@users.noreply.github.com>
2023-10-29 12:53:29 +0800

    Update index.js

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,9 +1,11 @@
 import ETH from './eth.png'
 import BNB from './bnb.png'
+import BNB from './Zeta.png'
 
 export const CURRENCY = {
   ETH,
   BNB,
+  ZETA,
 
   // ------------
 
@@ -14,4 +16,5 @@ export const CURRENCY = {
   42: ETH,
   56: BNB,
   97: BNB,
+  7001: ZETA,
 }
