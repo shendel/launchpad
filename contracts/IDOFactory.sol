7ed7c22e68e3fd0c262a6f3fd230d264c3a12eb5
shendel <im_shendel@inbox.ru>
2023-11-03 14:45:54 +0300

    fix - remix compile + verification

--- contracts/IDOFactory.sol
+++ contracts/IDOFactory.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "./openzeppelin/contracts/access/Ownable.sol";
+import "./openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 import "./IDOPool.sol";
 
--- contracts/TokenLockerFactory.sol
+++ contracts/TokenLockerFactory.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./openzeppelin/contracts/access/Ownable.sol";
+import "./openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./TokenLocker.sol";
 
 contract TokenLockerFactory is Ownable {
